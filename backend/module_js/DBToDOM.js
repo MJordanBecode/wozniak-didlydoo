9ebae8f02c0f1a86272b888e0e3fc5d8c0acff8b
Jiowolf <msel2lachts@gmail.com>
2025-04-02 15:39:08 +0200

    test main

---
+++ backend/module_js/DBToDOM.js
@@ -0,0 +1,8 @@
+import { getAllEvent } from "./getDB.js"
+
+export async function DBToDOM(){
+
+    const BODY_SELECT = document.querySelector("body");
+    
+}
+
--- backend/module_js/getDB.js
+++ backend/module_js/getDB.js
@@ -1,7 +1,67 @@
 
 
-export async function getDB(){
+export async function getAllEvent(){
+// fecth obj from local DataBase 
+    try{
+        let response = await fetch(`http://localhost:3000/api/events`,{
+            method: "GET",
+        });
+        const data = await response.json()
+        console.log(data)
+        return data
 
-    let dataDB = fetch(`http://localhost:3000/api/events`);
+    }catch(error){
+        console.log('Error : ',error);
+        alert(`Error : can't retrieve DataBase.`)
+    }
+
+}
+
+
+export async function getEventByID(id) {
     
+    try{
+        let response = await fetch(`http://localhost:3000/api/events/${id}`,{
+            method: "GET",
+        });
+        const data = await response.json()
+        console.log(data)
+        return data
+
+    }catch(error){
+        console.log('Error : ',error);
+        alert(`Error : can't retrieve DataBase.`)
+    }
+}
+
+export async function getAllAttendees(){
+
+    try{
+        let response = await fetch(`http://localhost:3000/api/attendees`,{
+            method: "GET",
+        });
+        const data = await response.json()
+        console.log(data)
+        return data
+
+    }catch(error){
+        console.log('Error : ',error);
+        alert(`Error : can't retrieve DataBase.`)
+    }
+}
+
+export async function getAttendeesByName(attendeeName) {
+    
+    try{
+        let response = await fetch(`http://localhost:3000/api/attendees/${attendeeName}`,{
+            method: "GET",
+        });
+        const data = await response.json()
+        console.log(data)
+        return data
+
+    }catch(error){
+        console.log('Error : ',error);
+        alert(`Error : can't retrieve DataBase.`)
+    }
 }
